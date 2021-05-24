98eadfa71b044f70af1edc15c16dae9bb0d58ecc
isanasan <yfcgpsebp@gmail.com>
2021-05-24 13:17:11 +0900

    delete print debug

--- routes/back.php
+++ routes/back.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'back';
-});
-
 Route::get('/', 'Back\DashboardController')->name('dashboard');
 
 Route::resource('Posts', 'Back/PostController')->except('show');
