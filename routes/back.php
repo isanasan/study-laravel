3bd49824f0d8589e1d98d69b234ac85d514fb7ad
isanasan <yfcgpsebp@gmail.com>
2021-05-25 10:05:05 +0900

    fix typo

--- routes/back.php
+++ routes/back.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Back\DashboardController')->name('dashboard');
 
-Route::resource('Posts', 'Back/PostController')->except('show');
+Route::resource('posts', 'Back\PostController')->except('show');
