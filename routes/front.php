b3c854192460859c990af425d649aaeded0ea6d8
isanasan <yfcgpsebp@gmail.com>
2021-05-26 18:26:42 +0900

    fix error "Target class [App\Http\Controllers\PostController] does not exist."

--- routes/front.php
+++ routes/front.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Front\PostController@index')->name('home');
 
-Route::resource('posts', 'PostController')->only(['index', 'show']);
+Route::resource('posts', 'Front\PostController')->only(['index', 'show']);
