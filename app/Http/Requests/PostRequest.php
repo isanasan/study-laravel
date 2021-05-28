29fb1876fe93b2557106e3aceaca4db7a238863c
isanasan <yfcgpsebp@gmail.com>
2021-05-28 10:14:33 +0900

    chenge validation post request title is too short

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -24,7 +24,7 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:2',
+            'title' => 'required|max:50',
             'body' => 'max:1000',
             'is_public' => 'required|numeric',
             'published_at' => 'required|date_format:Y-m-d H:i',
